fa2459a813d82d261d52baa23760712c31ee1259
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-16 21:35:11 -0700

    C programming project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+	static const char hello[] = "and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n";
+	fwrite(hello, sizeof(hello) - 1, 1, stdout);
+}
