b2b00ee26b4cbe684f9410fbc41c06647ab9f79b
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-19 10:00:59 -0700

    Alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - single digits of base 10 using putchar
+ * main - lowercase in reverse order
  * Return: Always 0
  */
 int main(void)
 {
-	int x;
-		for (x = 0; x < 10; x++)
-			putchar('0' + x);
+	char x;
+		for (x = 'z'; x > 'a'; x--)
+			putchar(x);
 		putchar('\n');
 		return (0);
 }
