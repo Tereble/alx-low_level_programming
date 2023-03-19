01bfcffee2d2e7ae7e022edd39cd8018e302d52e
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-19 08:57:38 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *  main - Entry
+ *  Retun: Always 0
+ */
+int main(void)
+{
+	int x;
+		for (x = "a", x <= "z", x++)
+		{
+			if (x != "e" && != "q")
+			{
+				putchar(x);
+			}
+		}
+		putchar('\n);
+		return (0);
+}
