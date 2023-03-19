eb2412fb29e0c0ffd9b7ac596d36403136abddf4
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-19 07:49:33 -0700

    correction on alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,11 @@
  */
 int main(void)
 {
-	for (int x = 'A'; x <= 'Z'; x++)
-	{
-		char small = tolower(x);
-			putchar(small);
-	}
+	int x;
+		for (x = 'A'; x <= 'Z'; x++)
+		{
+			char small = tolower(x);
+				putchar(small);
+		}
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,10 +5,11 @@
  */
 int main(void)
 {
-	for (int x = 'A'; x <= 'Z'; x++)
-	{
-		char small = tolower(x);
-			putchar(small, x);
-	}
+	int x;
+		for (x = 'A'; x <= 'Z'; x++)
+		{
+			char small = tolower(x);
+				putchar(small, x);
+		}
 	return (0);
 }
