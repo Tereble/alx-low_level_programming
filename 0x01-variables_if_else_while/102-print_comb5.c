3be92097ccc9bcc4144db16a20a61b4b97a6b0b7
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-19 12:54:53 -0700

    two two-digits combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int x;
 	int y;
-		for (x = 0; x < 10; x++)
+		for (x = 0; x <= 98; x++)
 		{
-			for (y = 0; y < 10; y++)
+			for (y = x + 1; y <= 99; y++)
 			{
 				putchar('0' + x);
 				putchar('0' + y);
-				if (x == 9 && y == 9)
+				if (x == 98 && y == 99)
 					continue;
 				putchar(',');
 				putchar(' ');
