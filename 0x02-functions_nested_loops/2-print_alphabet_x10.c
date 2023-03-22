905fc3c4fbac9a98f74aa2985211b214377ae6af
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-22 03:31:02 -0700

    correction on 2-print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - print the alphabet 10 times
- * Return: Always 0
+ * Return: On succes return 1
  */
 void print_alphabet_x10(void)
 {
 	char x, y;
-		for (x = 10; x < 10; x++)
+		for (x = 0; x < 10; x++)
 		{
-				for (y = 'a'; y <= 'z'; y++)
-				{
-					_putchar(y);
-				}
+			for (y = 'a'; y <= 'z'; y++)
+			{
+				_putchar(y);
+			}
 			_putchar('\n');
 		}
 }
