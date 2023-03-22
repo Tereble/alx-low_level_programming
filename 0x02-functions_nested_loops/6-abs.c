79f20b50d660ea884bc312c6d9f8ceaf05044280
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-21 17:21:31 -0700

    all

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs-prints the absolute value of an integer
+ *@x:the integer to compute its absolute value
+ *Return:returns the abs value
+ */
+ int _abs(int x)
+{
+	if (k >= 0)
+	{
+		return (x);
+	}
+	else 
+	{
+		return (-x);
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table
+ * Return: no return
+ */
+void times_table(void)
+{
+	int a, b, f;
+		for (a = 0; a <= 9; a++)
+		{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			f = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (f <= 9)
+			{
+				_putchar(32);
+				_putchar(f + 48);
+			}
+			else
+			{
+				_putchar((f / 10) + 48);
+				_putchar((f % 10) + 48);
+			}
+			_putchar('\n');
+		}
+}
