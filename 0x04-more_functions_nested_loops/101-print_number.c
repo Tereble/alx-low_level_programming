9a722d6de95ebff55b2700dd019bbe6ec50860be
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-23 20:19:01 -0700

    completion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,28 +7,15 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int x = n;
 		if (n < 0)
 		{
-			_putchar(45);
-			m = n * -1;
+			n *= -1;
+			x = n;
+			_putchar('_');
 		}
-		else
-		{
-			m = n;
-		}
-
-		d = m;
-		count = 1;
-
-		while (d > 9)
-		{
-			d /= 10;
-			count *= 10;
-		}
-
-		for (; count >= 1; count /= 10)
-		{
-			_putchar(((m / count) % 10) + 48);
-		}
+		x /= 10;
+		if (x != 0)
+			print_number(x);
+		_putchar((unsigned int) n % 10 + '0');
 }
