c23f0cfa4dade5c6d39a2c15789c556035f8e48f
Chukwuebuka Ojele <silasojele12@gmail.com>
2023-03-28 14:05:16 -0700

    swapping and lenght

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - swap values of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	strcpy(a, b);
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - will return the lenght of a string
+ * @s: string to be returned
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	_strlen(s);
+	return (0);
+}
